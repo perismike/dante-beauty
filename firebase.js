1588db50dfee8d87f980065029db6482a3bc3c64
perismike <kangatamike69@gmail.com>
2026-06-17 09:21:07 +0300

    Remove Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,11 +1,1 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
